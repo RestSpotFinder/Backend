ab67e45365ac826a514e5ae1b15234963ab09cd6
Gupuroom <cjsrnr1114@naver.com>
2024-03-29 15:53:21 +0900

    add: quarts - RouteSearchCount 생성하는 Job 추가 * 매월 말일 오전 9시에 다음 달 RouteSearchCount 생성

--- src/main/java/com/restspotfinder/common/quartz/JobSetting.java
+++ src/main/java/com/restspotfinder/common/quartz/JobSetting.java
@@ -1,6 +1,7 @@
 package com.restspotfinder.common.quartz;
 
 import com.restspotfinder.common.quartz.job.PlaceSearchCountCreateJob;
+import com.restspotfinder.common.quartz.job.RouteSearchCountCreateJob;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +25,17 @@ public class JobSetting {
         JobDetail jobDetail = buildJobDetail(PlaceSearchCountCreateJob.class, new HashMap());
         try {
             scheduler.scheduleJob(jobDetail, buildJobTrigger("0 55 23 * * ?"));
+        } catch (SchedulerException e){
+            log.error(e.getMessage());
+        }
+    }
+
+    // 매월 말일 오전 9시에 다음 달 RouteSearchCount 생성
+    @PostConstruct
+    public void createRouteSearchCount(){
+        JobDetail jobDetail = buildJobDetail(RouteSearchCountCreateJob.class, new HashMap());
+        try {
+            scheduler.scheduleJob(jobDetail, buildJobTrigger("0 0 9 L * ?"));
         } catch (SchedulerException e){
             log.error(e.getMessage());
         }
