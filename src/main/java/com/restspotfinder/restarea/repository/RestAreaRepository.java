e464f7c56c920c541e69c9f2504b862f4f0d508f
Gupuroom <cjsrnr1114@naver.com>
2024-05-03 15:21:35 +0900

    fix: RestAreaRepository - 기존: Join 이후 where 조건절 실행. 이럴 경우 로직상 필요없는 Join이 발생해 속도 저하 예상됨. - 변경: Where 조건절로 객체 특정 이후 해당 객체만 Join 진행

--- src/main/java/com/restspotfinder/restarea/repository/RestAreaRepository.java
+++ src/main/java/com/restspotfinder/restarea/repository/RestAreaRepository.java
@@ -3,6 +3,7 @@ package com.restspotfinder.restarea.repository;
 import com.restspotfinder.restarea.domain.RestArea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,15 +13,30 @@ public interface RestAreaRepository extends JpaRepository<RestArea, Long> {
      * 경로 LineString <-> 휴게소 좌표 비교 (500m 이내에 있는지 체크)
      * 출발지(LineString 의 첫번째 값)와 가까운 순으로 정렬
      ***/
+//    @Query(value = "SELECT r.* FROM rest_area r " +
+//            "JOIN route rt ON ST_DWithin( " +
+//            "    CAST(ST_SetSRID(rt.line_string, 4326) AS geography), " +
+//            "    CAST(ST_SetSRID(r.point, 4326) AS geography), " +
+//            "    500) " +
+//            "WHERE rt.route_id = :routeId " +
+//            "ORDER BY ST_Distance( " +
+//            "    CAST(ST_SetSRID(r.point, 4326) AS geography), " +
+//            "    CAST(ST_SetSRID(ST_StartPoint(rt.line_string), 4326) AS geography)) ASC"
+//            , nativeQuery = true)
+//    List<RestArea> findNearbyRoutes(long routeId);
+
     @Query(value = "SELECT r.* FROM rest_area r " +
-            "JOIN route rt ON ST_DWithin( " +
-            "    CAST(ST_SetSRID(rt.line_string, 4326) AS geography), " +
+            "JOIN ( " +
+            "    SELECT rt.line_string " +
+            "    FROM route rt " +
+            "    WHERE rt.route_id = :routeId " +
+            ") filtered_route ON ST_DWithin( " +
+            "    CAST(ST_SetSRID(filtered_route.line_string, 4326) AS geography), " +
             "    CAST(ST_SetSRID(r.point, 4326) AS geography), " +
             "    500) " +
-            "WHERE rt.route_id = :routeId " +
             "ORDER BY ST_Distance( " +
             "    CAST(ST_SetSRID(r.point, 4326) AS geography), " +
-            "    CAST(ST_SetSRID(ST_StartPoint(rt.line_string), 4326) AS geography)) ASC"
-            , nativeQuery = true)
-    List<RestArea> findNearbyRoutes(long routeId);
+            "    CAST(ST_SetSRID(ST_StartPoint(filtered_route.line_string), 4326) AS geography)) ASC",
+            nativeQuery = true)
+    List<RestArea> findNearbyRoutes(@Param("routeId") long routeId);
 }
