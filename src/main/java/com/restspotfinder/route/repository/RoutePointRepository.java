7bf132c9e89f69d99b4685d9076c5072243733ac
Gupuroom <cjsrnr1114@naver.com>
2024-06-11 15:46:18 +0900

    del: RoutePoint - 사용하지 않는 Domain 삭제

--- src/main/java/com/restspotfinder/route/repository/RoutePointRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restspotfinder.route.repository;
-
-import com.restspotfinder.route.domain.RoutePoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutePointRepository extends JpaRepository<RoutePoint, Long> {
-}
