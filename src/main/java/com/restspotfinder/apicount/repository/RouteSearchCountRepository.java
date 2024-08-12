7dacc145afa5056276497c1fc51d865c235e4578
Gupuroom <cjsrnr1114@naver.com>
2024-08-12 10:57:05 +0900

    fix: RouteSearchCount - 조회 시 없는 경우 새로 생성 하는 코드 추가  * Null일 경우 에러 발생, 서버로그 보기전까지 에러 원인 찾기 어려움. + 에러 방지

--- src/main/java/com/restspotfinder/apicount/repository/RouteSearchCountRepository.java
+++ src/main/java/com/restspotfinder/apicount/repository/RouteSearchCountRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public interface RouteSearchCountRepository extends JpaRepository<RouteSearchCount, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM RouteSearchCount c WHERE c.createdAt = :startOfMonth")
-    RouteSearchCount findByDateWithPessimisticLock(LocalDate startOfMonth);
+    Optional<RouteSearchCount> findByDateWithPessimisticLock(LocalDate startOfMonth);
 }
