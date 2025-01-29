f7b0fc40304ce06c5a891f7d9fae2848c3430ed4
Gupuroom <cjsrnr1114@naver.com>
2025-01-29 15:12:20 +0900

    add: log_module - 적용

--- src/main/java/com/restspotfinder/RestSpotFinderApplication.java
+++ src/main/java/com/restspotfinder/RestSpotFinderApplication.java
@@ -3,7 +3,7 @@ package com.restspotfinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.restspotfinder", "com.log_module"})
 public class RestSpotFinderApplication {
 
     public static void main(String[] args) {
