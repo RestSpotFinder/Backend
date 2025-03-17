1224c67f5f04b0e063487b34385f47e2f32b32e5
Gupuroom <cjsrnr1114@naver.com>
2025-03-17 10:06:04 +0900

    fix: 도메인 변경 - restspotfinder.site -> restspotfinder.kr

--- src/main/java/com/restspotfinder/common/config/WebConfig.java
+++ src/main/java/com/restspotfinder/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","https://www.restspotfinder.site","https://restspotfinder.site")
+                .allowedOrigins("http://localhost:5173","https://www.restspotfinder.kr","https://restspotfinder.kr")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*")
 //                .exposedHeaders("Custom-Header") // 클라이언트가 해당 Header에 접근하는 걸 허용
