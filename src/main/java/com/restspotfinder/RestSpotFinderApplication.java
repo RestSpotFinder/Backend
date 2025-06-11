973c6975ac9f8b053093e4cd62c5002b97814062
nuyeo <bluelayzxx@gmail.com>
2025-06-11 20:37:44 +0900

    feat: 주유소 데이터 업데이트 스케줄링
    - 매일 자정 업데이트

--- src/main/java/com/restspotfinder/RestSpotFinderApplication.java
+++ src/main/java/com/restspotfinder/RestSpotFinderApplication.java
@@ -2,7 +2,9 @@ package com.restspotfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.restspotfinder", "com.log_module", "com.exception_module"})
 public class RestSpotFinderApplication {
 
